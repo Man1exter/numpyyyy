f043e5348210213a7bc7049ba0dcfa66986640a2
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-19 16:43:56 +0100

    generating from file

--- learn_dir/misscellfile.py
+++ learn_dir/misscellfile.py
@@ -1,4 +1,9 @@
 import numpy as np
 
 #a = np.genfromtxt(data.txt,delimiter=',')
-print(a)
+#a = a.astype('int32')
+#print(a)
+
+# a > 50 => [FASLE,TRUE,ITD]
+
+# a[a > 50] => check array with those numbers
