b643371c9935db34e031c5c046be093b406b0c9d
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-20 13:50:19 +0100

    app to get results with matrices

--- matrices.py
+++ matrices.py
@@ -1,2 +1,34 @@
 import numpy as np
 
+while True:
+    try:
+        n, m = map(int, input("Podaj wymiary macierzy (n m): ").split())
+
+        print("Podaj elementy macierzy:")
+        elements = []
+        for i in range(n):
+            row = list(map(int, input().split()))
+            elements.append(row)
+
+        a = np.array(elements)
+
+        print("Macierz A:")
+        print(a)
+
+        at = np.transpose(a)
+        print("Macierz transponowana A^T:")
+        print(at)
+
+        ai = np.linalg.inv(a)
+        print("Macierz odwrotna A^-1:")
+        print(ai)
+
+        ad = np.linalg.det(a)
+        print("Wyznacznik macierzy det(A):")
+        print(ad)
+        break
+
+    except ValueError:
+        print("Błędne dane, podaj liczby całkowite")
+    except np.linalg.LinAlgError:
+        print("Macierz odwrotna nie istnieje, det(A) = 0.")
